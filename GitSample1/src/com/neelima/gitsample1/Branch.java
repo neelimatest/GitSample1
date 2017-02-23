e0a272b50a539eedde8a4cf28cce4fd316629b66
adarsh <adarsh@pl1usmgt0479nb.ww304.siemens.net>
2017-02-22 23:20:20 -0600

    Created Branch Pojo

---
+++ GitSample1/src/com/neelima/gitsample1/Branch.java
@@ -0,0 +1,26 @@
+package com.neelima.gitsample1;
+
+public class Branch {
+	
+	private String name;
+	
+	private long branchId;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public long getBranchId() {
+		return branchId;
+	}
+
+	public void setBranchId(long branchId) {
+		this.branchId = branchId;
+	}
+	
+
+}
